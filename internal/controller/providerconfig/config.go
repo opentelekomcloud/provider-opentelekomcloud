8509c116cfe65874956cccb8a38da4c9d476e1ad
anton-sidelnikov <a.sidelnikov@t-systems.com>
2024-07-11 17:42:06 +0200

    config fix

--- internal/controller/providerconfig/config.go
+++ internal/controller/providerconfig/config.go
@@ -9,24 +9,25 @@ import (
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/providerconfig"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/crossplane/upjet/pkg/controller"
+	"github.com/opentelekomcloud/provider-opentelekomcloud/apis/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // Setup adds a controller that reconciles ProviderConfigs by accounting for
 // their current usage.
 func Setup(mgr ctrl.Manager, o controller.Options) error {
-	name := providerconfig.ControllerName(v1beta1_old.ProviderConfigGroupKind)
+	name := providerconfig.ControllerName(v1beta1.ProviderConfigGroupKind)
 
 	of := resource.ProviderConfigKinds{
-		Config:    v1beta1_old.ProviderConfigGroupVersionKind,
-		UsageList: v1beta1_old.ProviderConfigUsageListGroupVersionKind,
+		Config:    v1beta1.ProviderConfigGroupVersionKind,
+		UsageList: v1beta1.ProviderConfigUsageListGroupVersionKind,
 	}
 
 	return ctrl.NewControllerManagedBy(mgr).
 		Named(name).
 		WithOptions(o.ForControllerRuntime()).
-		For(&v1beta1_old.ProviderConfig{}).
-		Watches(&v1beta1_old.ProviderConfigUsage{}, &resource.EnqueueRequestForProviderConfig{}).
+		For(&v1beta1.ProviderConfig{}).
+		Watches(&v1beta1.ProviderConfigUsage{}, &resource.EnqueueRequestForProviderConfig{}).
 		Complete(providerconfig.NewReconciler(mgr, of,
 			providerconfig.WithLogger(o.Logger.WithValues("controller", name)),
 			providerconfig.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name)))))
